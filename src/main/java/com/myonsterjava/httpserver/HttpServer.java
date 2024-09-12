7054c1eed5f074a7495e3191d63f493f3829e004
myonster <e0773753@u.nus.edu>
2024-09-12 14:40:16 +0800

    first commit project init

---
+++ src/main/java/com/myonsterjava/httpserver/HttpServer.java
@@ -0,0 +1,13 @@
+package com.myonsterjava.httpserver;
+
+/**
+ * 
+ * Driver Class for HTTP Server
+ * 
+ */
+
+public class HttpServer {
+    public static void main(String [] args) {
+        System.out.println("Server starting...");
+    }
+}
