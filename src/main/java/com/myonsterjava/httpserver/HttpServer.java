8de7f9eae1e93d3abed35766e57d87d0439f9a4b
myonster <e0773753@u.nus.edu>
2024-09-17 14:13:48 +0800

    static serving with server socket

--- src/main/java/com/myonsterjava/httpserver/HttpServer.java
+++ src/main/java/com/myonsterjava/httpserver/HttpServer.java
@@ -1,5 +1,15 @@
 package com.myonsterjava.httpserver;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import com.myonsterjava.httpserver.config.Configuration;
+
+import com.myonsterjava.httpserver.config.ConfigurationManager;
+
 /**
  * 
  * Driver Class for HTTP Server
@@ -9,5 +19,44 @@ package com.myonsterjava.httpserver;
 public class HttpServer {
     public static void main(String [] args) {
         System.out.println("Server starting...");
-    } 
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
+        Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
+
+        System.out.println("Using Port: " + conf.getPort());
+        System.out.println("Using Port: " + conf.getWebroot());
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+            
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            // TODO read
+
+            // TODO write
+            String html = "<html><head><title>Java HTTP server</title></head><body><h1>This page was served using Java HTTP server</h1></body></html>";
+
+            final String CRLF = "\n\r"; // 13, 10
+
+            String response = 
+                "HTTP/1.1 200 OK" + CRLF +  // Status line : HTTP VERSION RESPONSE_CODE RESPONSE_MESSAGE
+                "Content-Length" + html.getBytes().length + CRLF + // HEADER
+                    CRLF +
+                    html +
+                    CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+            
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
--- src/main/java/com/myonsterjava/httpserver/config/ConfigurationManager.java
+++ src/main/java/com/myonsterjava/httpserver/config/ConfigurationManager.java
@@ -1,5 +1,13 @@
 package com.myonsterjava.httpserver.config;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.myonsterjava.httpserver.util.Json;
+
 public class ConfigurationManager {
     /**
      * We are using singleton here
@@ -28,15 +36,48 @@ public class ConfigurationManager {
      *  Used to load a config file by the path provided
      */
 
-    public void loadConfigurationFile(String filePath) {
+    public void loadConfigurationFile(String filePath){
+        // FileReader fileReader = new FileReader(filePath);
+        FileReader fileReader = null;
+        try {
+            fileReader = new FileReader(filePath);
+        } catch (FileNotFoundException e) {
+            throw new HttpConfigurationException(e);
+        }
+        StringBuffer sb = new StringBuffer();
+        int i;
+        try {
+            while( ( i = fileReader.read()) != -1) {
+                sb.append((char) i);
+            }
+        } catch (IOException e) {
+            throw new HttpConfigurationException(e);
+        }
+        // JsonNode conf = Json.parse(sb.toString());
+        JsonNode conf = null;
+        try {
+            conf = Json.parse(sb.toString());
+        } catch (IOException e) {
+            throw new HttpConfigurationException("Error parsing the Configuration file", e);
+        }
 
+        //myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
+        try {
+            myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
+        } catch (JsonProcessingException e) {
+            throw new HttpConfigurationException("Error parsing the Configuration file, internal", e);
+        }
     }
 
     /**
      * Returns the current loaded configuration
      */
 
-    public void getCurrentConfiguration() {
+    public Configuration getCurrentConfiguration() {
+        if ( myCurrentConfiguration == null) {
+            throw new HttpConfigurationException("No Current Configuration Set");
+        }
 
+        return myCurrentConfiguration;
     }
 }
